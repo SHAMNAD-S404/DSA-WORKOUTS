9cb4346364f1b8b4200662b286e75fd52797d1f1
Shamnad <shamnadsakeer306@gmail.com>
2024-07-10 11:40:00 +0530

    added new array workout

--- Array/twoSum.js
+++ Array/twoSum.js
@@ -7,5 +7,31 @@
 //* the space used by the hash map.
 
 
- 
+ function twoSum(array,target){
 
+     let map={} //intializing empty obj to store numbers and their indices
+
+     for(let i=0; i<array.length; i++){
+
+        let diff = target - array[i];
+
+         // Check if the diff (needed number to reach target) exists in the map
+
+        if (map.hasOwnProperty(diff)) {
+
+            
+            return [map[diff],i] //Return the indices of the two numbers
+        }
+
+        //Store the current number and its index in the map
+        map[array[i]] = i ;
+     }
+     //if there is nothing then returning empty arraY
+     return []
+ }
+
+ let array = [10,7,13,40,50]
+ const target = 20;
+ 
+ //CALLING FUNCTION AND PRINTING OUT
+ console.log(twoSum(array,target))
